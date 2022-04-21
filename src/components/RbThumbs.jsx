95212e4a6d35f64bd797dca35218c17fcd05837e
JohnnyHiriart <johnnyhc@hotmail.com>
2022-04-21 15:40:27 +0200

    finalisation top quotes

--- src/components/RbThumbs.jsx
+++ src/components/RbThumbs.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const RbThumbs = () => {
   return (
-    <div className="flex items-center content-center justify-center pt-10 m-2 ">
+    <div className="flex items-center content-center justify-center pt-20 m-10">
       <div className="flex items-center justify-center text-4xl font-bold text-white rounded-full h-14 bg-redquote w-14">
         1
       </div>
-      <div className="flex content-center justify-center w-3/4 m-2 border-8 rounded-xl border-redquote bg-redquote bg-opacity-5">
+      <div className="flex content-center justify-center w-3/4 m-10 border-8 rounded-xl border-redquote bg-redquote bg-opacity-5">
         {/* box with quote */}
         <div>{/* first quote img */}</div>
         <div className="p-20">appel api classement</div>
