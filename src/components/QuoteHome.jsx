2dde1e7b339cfb1b8658e5afa11ed5a44e1ef451
fblaizoud <blaiz95@hotmail.com>
2022-04-21 20:00:59 +0200

    init carousel

--- src/components/QuoteHome.jsx
+++ src/components/QuoteHome.jsx
@@ -2,22 +2,27 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const QuoteHome = () => {
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
   // variable qui contient toutes les quotes
   const [quoteDay, setQuoteDay] = useState([]);
 
   // au chargement, aller chercher une quote
   useEffect(() => {
     axios
-      .get("https://favqs.com/api/qotd")
-      .then((res) => res.data.quote)
-      .then((data) => setQuoteDay(data))
+      .get("https://quote-garden.herokuapp.com/api/v3/quotes")
+      .then((res) => res.data.data)
+      .then((data) => setQuoteDay(data[getRandomInt(data.length)]))
       .catch((err) => console.log(err));
   }, []);
 
   return (
-    <div className="flex pt-48 flex-col text-center ">
-      <p>{quoteDay.body}</p>
-      <p className="font-bold">{quoteDay.author}</p>
+    <div className="flex pb-[10vh] flex-col text-center h-[15vh]">
+      <p>{quoteDay.quoteText}</p>
+      <p className="font-bold">{quoteDay.quoteAuthor}</p>
     </div>
   );
 };
