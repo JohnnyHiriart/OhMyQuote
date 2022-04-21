7e798c878145d941dec9634a5160b3f469413930
EstelleChardac <estelle.chardac@gmail.com>
2022-04-21 18:19:33 +0200

    added styled buttons and onFocus to category block > blocked by eslint

--- src/components/QuoteMe.jsx
+++ src/components/QuoteMe.jsx
@@ -10,11 +10,30 @@ const QuoteMe = () => {
         <NavBar />
       </div>
       <div className="flex flex-col items-center w-5/6 p-5 ml-28">
-        <div className="flex justify-center font-bold text-redquote text-7xl">
+        <div className="flex font-bold text-redquote text-7xl">
           <h1>QUOTE ME</h1>
         </div>
+        <div className="flex w-full ml-56">
+          <div className="flex flex-row justify-between w-1/5 p-2 mb-3 bg-slate-200">
+            <div>Category : ALL</div>
+            <button type="button">X</button>
+          </div>
+        </div>
         <div className="flex w-full">
-          <div className="flex flex-col w-full border-4 rounded-lg ml-28 h-96 bg-redquote bg-opacity-5 border-redquote" />
+          <div className="flex flex-row justify-between w-full border-4 rounded-lg ml-28 h-96 bg-redquote bg-opacity-5 border-redquote">
+            <div className="w-40">
+              <img
+                src="/src/assets/smal-logo-oh-my-quote2.png"
+                alt="opening_quote"
+              />
+            </div>
+            <div className="flex items-end w-40">
+              <img
+                src="/src/assets/smal-logo-oh-my-quote1.png"
+                alt="closing_quote"
+              />
+            </div>
+          </div>
           <div className="flex flex-col ml-5 space-y-28">
             <button type="button">
               <img src="/src/assets/thumbs-up_1f44d.png" alt="thumb_up" />
