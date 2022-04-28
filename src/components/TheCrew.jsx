7b5a6ad94aa1f5763b6929fd0e70a62a03a2f011
fblaizoud <blaiz95@hotmail.com>
2022-04-28 09:49:06 +0200

    all image urls have been updated for vercel

--- src/components/TheCrew.jsx
+++ src/components/TheCrew.jsx
@@ -6,26 +6,26 @@ import RedButton from "./RedButton";
 
 const crewItems = [
   {
-    image: "src/assets/86A5FC2D-3A82-4DF3-8F32-564B9583A941_1_201_a.jpg",
+    image: "/static/img/86A5FC2D-3A82-4DF3-8F32-564B9583A941_1_201_a.jpg",
     title: "Weshtelle: Our Quote Addict",
     quote: "'Dear world, I am leaving because I am bored'",
     signature: "George Sanders' epitaph",
   },
   {
-    image: "src/assets/2462f5c6-e17c-47d0-93e4-a760af066183.jpg",
+    image: "/static/img/2462f5c6-e17c-47d0-93e4-a760af066183.jpg",
     title: "Fred: Our Quote Editor",
     quote:
       "'It's better to keep quiet and look like a jerk than to talk and leave no doubt about it'",
     signature: "Pierre Desproges",
   },
   {
-    image: "src/assets/1649335671768.jpg",
+    image: "/static/img/1649335671768.jpg",
     title: "Lucas: Our Quote Selector",
     quote: "'Nothing is impossible, the word itself says 'I'm possible''",
     signature: "Audrey Hepburn",
   },
   {
-    image: "src/assets/IMG_20220325_104207.jpeg",
+    image: "/static/img/IMG_20220325_104207.jpeg",
     title: "Johnny: Our Quote Master",
     quote:
       "'After thirty years of studying female psychology, I still haven't found the answer to the big question: what do they really want?'",
